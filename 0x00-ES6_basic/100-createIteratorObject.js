c4118ccf1064fc11d02b08bd06315a284566d2b5
itboogie <udaknse111@gmail.com>
2023-07-28 23:08:23 +0000

    tasks

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,19 +1,10 @@
-xport default function createIteratorObject(report) {
-  const all = Object.values(report.allEmployees).reduce((a, b) => {
-    a.push(...b);
-    return a;
-  }, []);
-  let currIndex = 0;
-  const maxIndex = all.length;
-  return {
-    next() {
-      if (currIndex < maxIndex) {
-        const result = { value: all[currIndex], done: false };
-        currIndex += 1;
-        return result;
-      }
-      return { value: null, done: true };
-    },
-    [Symbol.iterator]: () => this.next(),
-  };
+export default function createIteratorObject(report) {
+  let allEmployees = [];
+  for (const item of Object.values(report.allEmployees)) {
+    allEmployees = [
+      ...allEmployees,
+      ...item,
+    ];
+  }
+  return allEmployees;
 }
