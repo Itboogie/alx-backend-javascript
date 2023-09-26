f61874569355dc1a5c2ce98d78577c245ebead51
itboogie <udaknse111@gmail.com>
2023-09-26 15:36:00 -0700

    tasks

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,9 @@
+unction getPaymentTokenFromAPI (success) {
+    return new Promise(function (resolve) {
+      if (success) {
+        resolve({ data: 'Successful response from the API' });
+      }
+    });
+}
+
+module.exports = getPaymentTokenFromAPI;
